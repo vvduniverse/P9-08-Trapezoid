5178d8e0dc9a7ffd6a6488f8b1aa10874e1f5f90
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-18 11:38:47 +0200

    Initial Commit

---
+++ P9-08-Trapezoid/P9_08_TrapezoidApp.swift
@@ -0,0 +1,17 @@
+//
+//  P9_08_TrapezoidApp.swift
+//  P9-08-Trapezoid
+//
+//  Created by Vahtee Boo on 18.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct P9_08_TrapezoidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
